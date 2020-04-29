4b483f1f00044cc15b3f84315e88f2d0b45438dc
MatheusChignolli <matheuschignolli@gmail.com>
2020-04-28 22:12:36 -0300

    Learning how to encrypt with some packages (MD5, bcrypt, mongoose-encrypt)

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// Hash and Encrypt
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
@@ -18,11 +20,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"]
-})
 
 const User = new mongoose.model("User", userSchema);
 
@@ -50,12 +47,13 @@ app.route("/login")
     }, function(err, result) {
       if(!err) {
         if(result) {
-          console.log(result);
-          if (result.password == password) {
-            res.render("Secrets");
-          } else {
-            res.render("Home");
-          }
+          bcrypt.compare(password, result.password, function (err, hash) {
+            if (hash === true) {
+              res.render("Secrets");
+            } else {
+              res.render("Home");
+            }
+          });
         } else {
           res.render("Home");
         }
@@ -74,20 +72,21 @@ app.route("/register")
 
     const email = req.body.username;
     const password = req.body.password;
-    
-    const newUser = new User({
-      email: email,
-      password: password
-    });
-          console.log(req.body);
 
-    newUser.save(function(err) {
-      if(!err) {
-        console.log("User created.")
-        res.render("Secrets");
-      } else {
-        console.log(err);
-      }
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+      const newUser = new User({
+        email: email,
+        password: hash
+      });
+
+      newUser.save(function (err) {
+        if (!err) {
+          console.log("User created.")
+          res.render("Secrets");
+        } else {
+          console.log(err);
+        }
+      });
     });
 
   });
